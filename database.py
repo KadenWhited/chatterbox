368bf05daa7e3dd8f483afe970b2fa0d8c548b2b
KadenWhited <kadenwhited@gmail.com>
2026-01-19 00:06:49 -0600

    database_url config

--- database.py
+++ database.py
@@ -2,8 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./dev.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./dev.db") or "sqlite:///./dev.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgress://", "postgresql://", 1)
+
+print("DATABASE_URL (used by SQLAlchemy):", DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
